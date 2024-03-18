0f3bb9d20be56e6d597db201855fbe02cda57bdd
Kyle Angelich <kyleangelich1@gmail.com>
2024-03-18 15:26:52 -0700

    Refactor connection.js

--- server/db/connection.js
+++ server/db/connection.js
@@ -2,26 +2,25 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config({ path: '../../config.env' });
 
-const connectToDatabase = async () => {
-    const uri = process.env.MONGODB_URI || "";
-    const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-        }
-    });
-    return client;
-}
+const uri = process.env.MONGODB_URI;
 
-async function run() {
-  const client = await connectToDatabase();
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+export async function connectToDatabase() {
   try {
     await client.connect();
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    await client.close();
+    return client.db("EarthquakeDataCluster");
+  } catch (err) {
+    console.error("Failed to connect to MongoDB:", err);
+    throw err; // Rethrow the error to be handled by the caller
   }
 }
-run().catch(console.dir);
+connectToDatabase();
