b1c3a4902acb9df76013e33040ada951710d6060
Kyle Angelich <kyleangelich1@gmail.com>
2024-03-25 23:17:01 -0700

    Add mongoose schema and model

---
+++ server/models/earthquakeModel.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const earthquakeSchema = new mongoose.Schema({
+    magnitude: Number,
+    location: String,
+    time: Date,
+    coordinates: [Number]
+});
+
+// Create a Mongoose model based on the schema
+const Earthquake = mongoose.model('Earthquake', earthquakeSchema);
+
+export default Earthquake;
